e34e807c4ee640c0e2bcdcbc22d0896deb23301d
OnigiriKing <oooNikita87@gmail.com>
2023-07-22 15:49:41 +0700

    About page update

--- src/pages/AboutPage/About.jsx
+++ src/pages/AboutPage/About.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import "./style.css";
+import aboutImg from "../../img/aboutImg.png";
+import aboutImg2 from "../../img/aboutImg2.png";
+import aboutImg3 from "../../img/aboutImg3.png";
 
 
 export default function AboutPage() {
     
     return (
-        <div id="about-screen">
-        
+      <div id="about-screen">
+        <div className="about-image">
+          <img src={aboutImg3} />
         </div>
-    )
+        <div className="about-des">
+          <p>ABOUT ME</p>
+          <p>Dedicated Front-end React Developer based in Bangkok, Thailand.</p>
+          <p>
+            text
+          </p>
+        </div>
+      </div>
+    );
 }
