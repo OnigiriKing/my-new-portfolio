3d4daa2d369935a988b1b646e1f84108c36f70c0
OnigiriKing <oooNikita87@gmail.com>
2023-07-22 13:59:14 +0700

    Tech stack is react, font page ready

--- src/pages/StartPage/StartPage.jsx
+++ src/pages/StartPage/StartPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.css";
 import Typed from "typed.js";
+import { skillsSvg } from "../../svg/skillsSvg";
 
 function TypeComponent() {
   const el = React.useRef(null);
@@ -30,20 +31,29 @@ export default function StartPage() {
   return (
     <div id="start-screen">
       <div className="start-screen-wrapper">
-        <div className="start-screen-des">
-          <div className="auto-typing">
-            <TypeComponent />
+        <div className="text-photo-wrapper">
+          <div className="start-screen-des">
+            <div className="auto-typing">
+              <TypeComponent />
+            </div>
+            <p>
+              Hello, I'm Nick – a dedicated Front-end React Developer based in
+              Bangkok, Thailand.
+            </p>
           </div>
-          <p>
-            Hi, I'm Nick. A passionate Front-end React Developer based in
-            Bangkok, Thailand.
-          </p>
+          <div className="start-screen-photo"></div>
         </div>
-        <div className="start-screen-photo"></div>
-      </div>
-      <div className="start-screen-skills">
-        <div>Tech Stack</div>
-        <div>React, js, css etc</div>
+        <div className="start-screen-skills">
+          <p>Tech Stack</p>
+          <div>
+            <ul className="skill-list">
+              <li className="skill">{skillsSvg(40).react}</li>
+              <li className="skill">{skillsSvg(40).javaScript}</li>
+              <li className="skill">{skillsSvg(40).css}</li>
+              <li className="skill">{skillsSvg(40).html}</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
