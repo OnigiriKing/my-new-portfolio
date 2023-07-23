c4f7336918f319f66aacdce531239e84237ad222
OnigiriKing <oooNikita87@gmail.com>
2023-07-23 10:12:28 +0700

    Text / picture added

--- src/pages/AboutPage/About.jsx
+++ src/pages/AboutPage/About.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./style.css";
 import aboutImg from "../../img/aboutImg.png";
-import aboutImg2 from "../../img/aboutImg2.png";
-import aboutImg3 from "../../img/aboutImg3.png";
+// import aboutImg2 from "../../img/aboutImg2.png";
+// import aboutImg3 from "../../img/aboutImg3.png";
 
 
 export default function AboutPage() {
@@ -10,13 +10,24 @@ export default function AboutPage() {
     return (
       <div id="about-screen">
         <div className="about-image">
-          <img src={aboutImg3} />
+          <img src={aboutImg} />
         </div>
         <div className="about-des">
           <p>ABOUT ME</p>
-          <p>Dedicated Front-end React Developer based in Bangkok, Thailand.</p>
           <p>
-            text
+            Passionate Front-End React Developer based in Bangkok, Thailand.
+          </p>
+          <p>
+            Greetings! I'm a dynamic junior front-end React developer,
+            proficient in React, JavaScript, CSS, and HTML. With a true passion
+            for clean and efficient code, I thrive on turning ideas into
+            engaging user experiences. Eager to stay updated with the latest
+            industry trends, I am committed to continuously honing my skills to
+            deliver top-notch web applications. I take pride in my
+            problem-solving abilities and collaborative mindset, making me an
+            asset to any development team. Let's connect and explore how I can
+            bring value to your projects. Together, we can craft exceptional
+            digital experiences!
           </p>
         </div>
       </div>
