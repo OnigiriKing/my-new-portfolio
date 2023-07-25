4a41128c99d1bc316ec147d69229d814e05764df
OnigiriKing <oooNikita87@gmail.com>
2023-07-25 13:45:55 +0700

    Close menu function created and implimented

--- src/pages/DropMenu/DropMenu.jsx
+++ src/pages/DropMenu/DropMenu.jsx
@@ -6,17 +6,27 @@ import { closeButton } from "../../svg/headerSvg";
 export default function DropMenu() {
 
   function closeMenu() {
-
+    function close() {
+      document.querySelector(".drop-menu").classList.remove("menu-open");
+    }
+    return {
+      onClick: function () {
+        close();
+      },
+    };
   }
 
 
+
     return (
-      <div className="drop-menu ">
-        <div className="menu-close-button">{closeButton(50)}</div>
-        <a href="#start-screen">Home</a>
-        <a href="#about-screen">About</a>
-        <a href="#works-screen">Projects</a>
-        <a href="#contacts-screen">Contacts</a>
+      <div className="drop-menu menu-open">
+        <div className="menu-close-button" {...closeMenu()}>
+          {closeButton(50)}
+        </div>
+        <a href="#start-screen" {...closeMenu()}>Home</a>
+        <a href="#about-screen" {...closeMenu()}>About</a>
+        <a href="#works-screen" {...closeMenu()}>Projects</a>
+        <a href="#contacts-screen" {...closeMenu()}>Contacts</a>
       </div>
     );
 }
