8a1ffa3a56f05fd63afbc07898e119341d81da24
OnigiriKing <oooNikita87@gmail.com>
2023-07-25 14:36:31 +0700

    about page text update

--- src/pages/AboutPage/About.jsx
+++ src/pages/AboutPage/About.jsx
@@ -17,16 +17,13 @@ export default function AboutPage() {
             Passionate Front-End React Developer based in Bangkok, Thailand 🇹🇭
           </p>
           <p>
-            Greetings! I'm a dynamic junior front-end React developer,
+            Hello! I'm a junior front-end developer,
             proficient in React, JavaScript, CSS, and HTML. With a true passion
             for clean and efficient code, I thrive on turning ideas into
-            engaging user experiences. Eager to stay updated with the latest
-            industry trends, I am committed to continuously honing my skills to
-            deliver top-notch web applications. I take pride in my
-            problem-solving abilities and collaborative mindset, making me an
+            engaging user experiences. My
+            problem-solving abilities and collaborative mindset, allows me to be a good
             asset to any development team. Let's connect and explore how I can
-            bring value to your projects. Together, we can craft exceptional
-            digital experiences!
+            bring value to your projects.
           </p>
         </div>
       </div>
