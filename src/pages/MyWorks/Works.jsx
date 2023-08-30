16806cc8a0f7a547351ddbb9545c0f140420bb93
OnigiriKing <oooNikita87@gmail.com>
2023-08-30 22:27:54 +0700

    KumoMarket is added

--- src/pages/MyWorks/Works.jsx
+++ src/pages/MyWorks/Works.jsx
@@ -1,19 +1,14 @@
 import React from "react";
-import "./style.css"
+import "./style.css";
 import { portfolioSvg } from "../../svg/portfolioSvg";
-import Nike from "../../img/Nike.png";
 import projectsList from "./data/projects";
 
-
 export default function WorksScreen() {
-
-
   function DisplayProjects() {
-
-    Object.keys(projectsList).map((key) => {
-      const el = projectsList[key]
+    return Object.keys(projectsList).map((key) => {
+      const el = projectsList[key];
       return (
-        <div className="portfolio-website" key={el.name}>
+        <div className="portfolio-website" key={el.key}>
           <div className="website-img">
             <a href={el.demoLink} target="_blank" rel="noreferrer">
               <img src={el.img} alt="Website" />
@@ -21,9 +16,7 @@ export default function WorksScreen() {
           </div>
           <div className="website-info">
             <p>{el.name}</p>
-            <p>
-             {el.des}
-            </p>
+            <p>{el.des}</p>
             <div className="skills-used">
               <div>{el.language}</div>
               <div> {el.css}</div>
@@ -32,72 +25,27 @@ export default function WorksScreen() {
               <a href={el.codeLink} target="_blank" rel="noreferrer">
                 Code{portfolioSvg(50).github}
               </a>
-              <a
-                href={el.demoLink}
-                target="_blank"
-                rel="noreferrer"
-              >
+              <a href={el.demoLink} target="_blank" rel="noreferrer">
                 Live Demo{portfolioSvg(50).link}
               </a>
             </div>
           </div>
         </div>
       );
+    });
+  }
 
-      
-    })
-
-  };
-
-
-
-
-    return (
-      <div id="works-screen">
-        <div className="portfolio-wrapper">
-          <div className="portfolio-text">
-            <p>PORTFOLIO</p>
-            <p>Embracing Individuality Across Every Project</p>
-          </div>
-          <div className="portfolio-projects">
-            <div className="portfolio-website">
-              <div className="website-img">
-                <a
-                  href="https://www.nike.com/th/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img src={Nike} alt="nikeWebsite"/>
-                </a>
-              </div>
-              <div className="website-info">
-                <p>Nike 🏃</p>
-                <p>
-                  Nike's website is the official online platform for the
-                  renowned sportswear and athletic footwear brand. Shop the
-                  latest collections and iconic products for sports, fitness,
-                  and style.
-                </p>
-                <div className="skills-used">
-                  <div>React</div>
-                  <div> Vanila Css</div>
-                </div>
-                <div className="website-links">
-                  <a href="Link" target="_blank" rel="noreferrer">
-                    Code{portfolioSvg(50).github}
-                  </a>
-                  <a
-                    href="https://www.nike.com/th/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Live Demo{portfolioSvg(50).link}
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
+  return (
+    <div id="works-screen">
+      <div className="portfolio-wrapper">
+        <div className="portfolio-text">
+          <p>PORTFOLIO</p>
+          <p>Embracing Individuality Across Every Project</p>
+        </div>
+        <div className="portfolio-projects">
+          <DisplayProjects />
         </div>
       </div>
-    );
+    </div>
+  );
 }
---
+++ src/pages/MyWorks/data/projects.js
@@ -0,0 +1,16 @@
+import kumoMarket from "./img/kumoMarket.png";
+
+const projectsList = {
+  KumoMarket: {
+    name: "Kumo Market ☁️",
+    img: kumoMarket,
+    des: "Kumo Market is a minimalist e-commerce platform designed for straightforward and efficient online shopping. Discover a carefully curated selection of products in a clean and user-friendly environment. ",
+    language: "React",
+    css: "SCSS",
+    codeLink: "https://github.com/OnigiriKing/kumo-market",
+    demoLink: "https://kumo-market.netlify.app",
+    key: "kumoMarket",
+  },
+};
+
+export default projectsList;
