a14b7194a4fb43ad6f012c61ab1a1b209098caa2
OnigiriKing <oooNikita87@gmail.com>
2023-08-30 22:13:13 +0700

    DisplayProjects component created

--- src/pages/MyWorks/Works.jsx
+++ src/pages/MyWorks/Works.jsx
@@ -2,9 +2,52 @@ import React from "react";
 import "./style.css"
 import { portfolioSvg } from "../../svg/portfolioSvg";
 import Nike from "../../img/Nike.png";
+import projectsList from "./data/projects";
 
 
 export default function WorksScreen() {
+
+
+  function DisplayProjects() {
+
+    Object.keys(projectsList).map((key) => {
+      const el = projectsList[key]
+      return (
+        <div className="portfolio-website" key={el.name}>
+          <div className="website-img">
+            <a href={el.demoLink} target="_blank" rel="noreferrer">
+              <img src={el.img} alt="Website" />
+            </a>
+          </div>
+          <div className="website-info">
+            <p>{el.name}</p>
+            <p>
+             {el.des}
+            </p>
+            <div className="skills-used">
+              <div>{el.language}</div>
+              <div> {el.css}</div>
+            </div>
+            <div className="website-links">
+              <a href={el.codeLink} target="_blank" rel="noreferrer">
+                Code{portfolioSvg(50).github}
+              </a>
+              <a
+                href={el.demoLink}
+                target="_blank"
+                rel="noreferrer"
+              >
+                Live Demo{portfolioSvg(50).link}
+              </a>
+            </div>
+          </div>
+        </div>
+      );
+
+      
+    })
+
+  };
 
 
 
