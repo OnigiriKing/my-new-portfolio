d95d4c77024f1a5c256903cf3f3cf7c8429c74d7
OnigiriKing <oooNikita87@gmail.com>
2025-09-14 10:30:10 +0900

    bug fix netlify

--- src/pages/MyWorks/data/projects.js
+++ src/pages/MyWorks/data/projects.js
@@ -6,7 +6,7 @@ const projectsList = {
   KumoMarket: {
     name: "Kumo Market ☁️",
     img: kumoMarket,
-    des: "Kumo Market is a minimalist e-commerce platform designed for straightforward and efficient online shopping. Discover a carefully curated selection of products in a clean and user-friendly environment. ",
+    des: "Kumo Market is a minimalist e-commerce platform designed for straightforward and efficient online shopping. Discover a carefully curated selection of products in a clean and user-friendly environment.",
     language: "React",
     css: "SCSS",
     codeLink: "https://github.com/OnigiriKing/kumo-market",
